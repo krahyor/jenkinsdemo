4460a59182ebfed2fe0841e947b6352d51bee710
krahyor <gimgim611@gmail.com>
2025-08-27 11:43:31 +0700

    🦄 refactor:add student id

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello PSU Students from Jenkins!"
+    return "Hello PSU Students 6510110218 from Jenkins!"
 
 
 if __name__ == "__main__":
